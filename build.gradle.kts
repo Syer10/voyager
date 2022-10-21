e9ebb6c1de7c7f9bcc4082ac5313e87a197a94d6
Syer10 <syer10@users.noreply.github.com>
2022-10-21 19:44:49 -0400

    Start publishing my version

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,3 @@
-import com.vanniktech.maven.publish.MavenPublishPluginExtension
-import com.vanniktech.maven.publish.SonatypeHost
-
 buildscript {
     repositories {
         mavenCentral()
@@ -19,13 +16,27 @@ buildscript {
 
 subprojects {
     apply(plugin = "org.jlleitschuh.gradle.ktlint")
-    plugins.withId("com.vanniktech.maven.publish") {
-        configure<MavenPublishPluginExtension> {
-            sonatypeHost = SonatypeHost.S01
-        }
-    }
 }
 
 tasks.register<Delete>("clean") {
     delete(rootProject.buildDir)
+
+    // Read in the signing.properties file if it is exists
+    val signingPropsFile = rootProject.file("release/signing.properties")
+    if (signingPropsFile.exists()) {
+        java.util.Properties().apply {
+            signingPropsFile.inputStream().use {
+                load(it)
+            }
+        }.forEach { key1, value1 ->
+            val key = key1.toString()
+            val value = value1.toString()
+            if (key == "signing.secretKeyRingFile") {
+                // If this is the key ring, treat it as a relative path
+                project.ext.set(key, rootProject.file(value).absolutePath)
+            } else {
+                project.ext.set(key, value)
+            }
+        }
+    }
 }
